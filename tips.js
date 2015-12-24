b8a17ff3bcfdc7c671be01896adcf084a57aa386
Luke Larsen <luke@lukelarsen.com>
2015-12-24 10:31:35 -0700

    add support for multiple close butons on the tips.

--- tips.js
+++ tips.js
@@ -1,19 +1,22 @@
 'use strict';
 
 window.addEventListener('load', function () {
+    var tip_containers = document.getElementsByClassName('tip-container');
     var tip_triggers = document.getElementsByClassName('tip-trigger');
-    var tip_closers = document.getElementsByClassName('tip__close');
 
-    for (var i = 0; i < tip_triggers.length; i++) {
-        tip_triggers[i].addEventListener('click', function () {
+    for (var i = 0; i < tip_containers.length; i++) {
+        tip_containers[i].firstElementChild.addEventListener('click', function () {
             var the_tip = this.parentNode.getElementsByClassName('tip')[0];
             the_tip.classList.add('tip--visible');
         });
 
-        tip_closers[i].addEventListener('click', function () {
-            var the_tip = this.parentNode.parentNode.getElementsByClassName('tip')[0];
-            the_tip.classList.remove('tip--visible');
-        });
+        var tip_closers = tip_containers[i].getElementsByClassName('tip__close');
 
+        for (var j = 0; j < tip_closers.length; j++) {
+            tip_closers[j].addEventListener('click', function () {
+                var the_tip = this.parentNode.parentNode.getElementsByClassName('tip')[0];
+                the_tip.classList.remove('tip--visible');
+            });
+        }
     }
 });
